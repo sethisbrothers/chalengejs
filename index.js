8b70643a40a27633917ec5cdcbe1091ae37b2166
sethisbrothers <sodjisethkokou@gmail.com>
2022-05-06 11:42:27 +0000

    challengejs

---
+++ index.js
@@ -0,0 +1,20 @@
+function Envoyer() {
+    let name = document.querySelector("#name");
+    let mail = document.querySelector("#mail");
+    let number = document.querySelector("#number");
+    let choice = document.querySelector("#choice");
+    let commenter = document.querySelector("#commenter");
+
+
+    let mes = document.querySelector("#message1");
+    let mess = document.querySelector("#message2");
+    let messa = document.querySelector("#message3");
+    let messag = document.querySelector("#message4");
+    let message = document.querySelector("#message5");
+
+    mes.innerHTML = name.value;
+    mess.innerHTML = mail.value;
+    messa.innerHTML = number.value;
+    messag.innerHTML = choice.value;
+    message.innerHTML = commenter.value;
+}
